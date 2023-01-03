35d044d12b016159bbb0f9dfb3358dc30f4ce596
Lyle Dean <dean@lyle.dev>
2023-01-03 00:48:12 +0000

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,4 @@
 use std::env;
 
 fn main() {
-    env::set_var("DYLD_LIBRARY_PATH", "/Users/lyledean/rust/rust2git/libgit2-sys/target/source-v1.5.0/build");
-    println!(r"DYLD_LIBRARY_PATH=/Users/lyledean/rust/rust2git/libgit2-sys/target/source-v1.5.0/build");
-    println!(r"cargo:rustc-link-search=native=/Users/lyledean/rust/rust2git/libgit2-sys/target/source-v1.5.0/build");
 }
